962ade9f03c1f0cc6f5774071891fcdd0e811c5e
RajshekarReddyM <rajashekarreddy0797@gmail.com>
2023-03-17 19:19:28 +0530

    hh

--- React_Projects/gpt/src/App.js
+++ React_Projects/gpt/src/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Blog, Features, Footers, Header, Possibility, WhatGPT3 } from './containers';
+import { Brand, CTA, Navbar } from './components';
 
+import './App.css'
 const App = () => {
   return (
-    <div>
-      <h1>Hello GPT</h1>
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar/>
+        <Header/>
+      </div>
+      <Brand/>
+      <WhatGPT3/>
+      <Features/>
+      <Possibility/>
+      <CTA/>
+      <Blog/>
+      <Footers/>
     </div>
   )
 }
--- React_Projects/gpt/src/components/navbar/Navbar.jsx
+++ React_Projects/gpt/src/components/navbar/Navbar.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import './navbar.css';
+import { RiMenu3lLine, RiCloselin } from 
 
 const Navbar = () => {
     return(
-        <div> Navbar </div >
+        <div className='gpt3__navbar'> 
+            Navbar 
+            <div className='gpt3__navbar-links'>
+                <div className='gpt3__navbar-links_logo'></div>
+                <img src={logo} alt="logo" />
+            </div>
+        </div >
     )
 }
 
